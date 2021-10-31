14f55a6eb87652fa178f0a68aab4d81adc2a9f03
Tim <pasiontimothy@gmail.com>
2021-10-30 19:31:44 -0600

    feat: started userPostModel
    still developing the posts

--- app/lib/domain/models/user_post_model.dart
+++ app/lib/domain/models/user_post_model.dart
@@ -1,1 +1,72 @@
-class UserPostModel {}
+import 'dart:convert';
+
+
+class UserPostModel {
+  final String userId;
+  final String title;
+  final String description;
+  final String url;
+  final String imageUrl;
+  final String imagePath;
+  final String type;
+  final String user;
+  final String category; 
+ // final String updatedDate;
+  String date;
+  
+  UserPostModel({
+    this.user ="",
+    required this.userId,
+    required this.category,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.imageUrl,
+    required this.imagePath,
+    required this.type,
+    this.date ="",
+  });
+
+//Take saved values in local variable and return a map
+    Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'username': user,
+      'category': category,
+      'title': title,
+      'description': description,
+      'url': url,
+      'imageUrl': imageUrl,
+      'imagePath': imagePath,
+      'type': type,
+      'dateCreated': date,
+    };
+  }
+
+// named constuctor: retrieve values and save it to local variables
+  factory UserPostModel.fromMap(Map<String, dynamic> map){
+    return UserPostModel(
+      userId: map["userId"],
+      user: map["username"],
+      category: map["category"],
+      title: map["title"],
+      description: map["description"],
+      url: map["url"],
+      imageUrl: map["imageUrl"],
+      imagePath: map["imagePath"],
+      type: map["type"],
+      date: map["dateCreated"],
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
