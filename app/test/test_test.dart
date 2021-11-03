ebe000945ec33820a3e70c0ce1f10378fcd5a596
Mohamed Bashir <mohamedbashir@Mohameds-MacBook-Pro.local>
2021-11-02 22:05:50 -0300

    added diaglog and navigation service

--- app/test/test_test.dart
+++ app/test/test_test.dart
@@ -4,9 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 main() {
   test('Counter value should be incremented', () {
     final counter = Counter();
-
     counter.increment();
-
     expect(counter.value, 1);
   });
 }
