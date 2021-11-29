cb8d5a240c0fc9a6062cb26068e1885e75518dc7
Anupras Burokas <abe360@uregina.ca>
2021-11-29 12:34:56 -0600

    make change

--- app/test/test_test.dart
+++ app/test/test_test.dart
@@ -96,4 +96,45 @@ void main() {
       dateCreated: '11/04/2021',
     );
   }
+  try {
+  UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: "barry.allen@example.com",
+    password: "SuperSecretPassword!"
+  );
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'user-not-found') {
+    print('No user found for that email.');
+  } else if (e.code == 'wrong-password') {
+    print('Wrong password provided for that user.');
+  }
+}
+try {
+  UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: "barry.allen@example.com",
+    password: "SuperSecretPassword!"
+  );
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'weak-password') {
+    print('The password provided is too weak.');
+  } else if (e.code == 'email-already-in-use') {
+    print('The account already exists for that email.');
+  }
+} catch (e) {
+  print(e);
+}
+User? user = FirebaseAuth.instance.currentUser;
+
+if (user != null && !user.emailVerified) {
+  var actionCodeSettings = ActionCodeSettings(
+      url: 'https://www.example.com/?email=${user.email}',
+      dynamicLinkDomain: 'example.page.link',
+      androidPackageName: 'com.example.android',
+      androidInstallApp: true,
+      androidMinimumVersion: '12',
+      iOSBundleId: 'com.example.ios',
+      handleCodeInApp: true,
+  );
+
+  await user.sendEmailVerification(actionCodeSettings);
+}
 }
